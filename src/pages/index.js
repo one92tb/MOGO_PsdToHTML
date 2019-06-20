6c0b5855bddcb2279841fa0b1a534f29c9525164
one92tb <one92tb@gmail.com>
2019-06-20 17:34:15 +0200

    install loadable and change import components for loadable import components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from "react"
 import Layout from "../components/layout"
+import loadable from "@loadable/component"
 import styled from "styled-components"
-import Intro from "../components/Intro/intro"
-import About from "../components/About/about"
-import Statistics from "../components/Statistics/statistics"
-import Services from "../components/Services/services"
-import Feature from "../components/Feature/feature"
-import WhatWeDo from "../components/WhatWeDo/whatWeDo"
-import Quote from "../components/Quote/quote"
-import WhoWeAre from "../components/WhoWeAre/whoWeAre"
+
+const Intro = loadable(() => import("../components/Intro/intro"))
+const About = loadable(() => import("../components/About/about"))
+const Statistics = loadable(() => import("../components/Statistics/statistics"))
+const Services = loadable(() => import("../components/Services/services"))
+const Feature = loadable(() => import("../components/Feature/feature"))
+const WhatWeDo = loadable(() => import("../components/WhatWeDo/whatWeDo"))
+const Quote = loadable(() => import("../components/Quote/quote"))
+const WhoWeAre = loadable(() => import("../components/WhoWeAre/whoWeAre"))
 
 const Container = styled.div`
   width: 100%;
@@ -29,7 +31,7 @@ class IndexPage extends React.Component {
     return (
       <Layout>
         <Container>
-          <Intro references={this.references} setRef={this.setRef}/>
+          <Intro references={this.references} setRef={this.setRef} />
           <About setRef={this.setRef} />
           <Statistics />
           <Services setRef={this.setRef} />
